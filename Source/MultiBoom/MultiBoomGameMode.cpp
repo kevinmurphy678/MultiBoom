63694809ab3da26fe17edfa93c225025161a1545
kevinmurphy678 <kevinmurphy678@gmail.com>
2016-07-25 03:27:41 -0400

    yay

---
+++ Source/MultiBoom/MultiBoomGameMode.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "MultiBoom.h"
+#include "MultiBoomGameMode.h"
+#include "MultiBoomCharacter.h"
+
+AMultiBoomGameMode::AMultiBoomGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	//if (PlayerPawnBPClass.Class != NULL)
+	//{
+	//	DefaultPawnClass = PlayerPawnBPClass.Class;
+	//}
+	
+}
